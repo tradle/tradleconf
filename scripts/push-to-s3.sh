851381c297b346bd50d0a33dcd63e3619183aea7
Mark Vayngrib <mark@tradle.io>
2017-12-27 16:08:41 -0500

    feat: prevent push to s3 for files that haven't changed

--- scripts/push-to-s3.sh
+++ scripts/push-to-s3.sh
@@ -10,11 +10,23 @@ do
   if [ -f "$key" ]
   then
     echo "pushing $key"
-    eval $s3 put-object \
+    obj_head=$(eval $s3 head-object \
       --bucket "$bucket" \
-      --key "$key" \
-      --content-type "application/json" \
-      --body "$key"
+      --key "$key" || echo '{}')
+
+    # 2nd jq is to strip quotes
+    etag=$(echo "$obj_head" | jq .ETag --raw-output | jq . --raw-output)
+    obj_md5=$(md5sum "$key" | awk '{ print $1 }')
+    if [ "$etag" == "$obj_md5" ]
+    then
+      echo "$key content has not changed, skipping push"
+    else
+      eval $s3 put-object \
+        --bucket "$bucket" \
+        --key "$key" \
+        --content-type "application/json" \
+        --body "$key"
+    fi
   else
     echo "$key doesn't exist, not pushing"
   fi
