4672f51fe12466c1ec5f4876a03257b2d43c2b47
Mark Vayngrib <mark@tradle.io>
2017-12-28 15:38:27 -0500

    fix: cross-platform md5

--- scripts/push-to-s3.sh
+++ scripts/push-to-s3.sh
@@ -3,12 +3,22 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+getmd5() {
+  if [ command -v md5 ]; then
+    md5 -q "$key"
+  elif [ command -v md5sum ]; then
+    md5sum "$key" | awk '{ print $1 }'
+  else
+    # give up
+    echo ""
+  fi
+}
+
 source ./scripts/load-env.sh
 
 for key in "$@"
 do
-  if [ -f "$key" ]
-  then
+  if [ -f "$key" ]; then
     echo "pushing $key"
     obj_head=$(eval $s3 head-object \
       --bucket "$bucket" \
@@ -17,8 +27,7 @@ do
     # 2nd jq is to strip quotes
     etag=$(echo "$obj_head" | jq .ETag --raw-output | jq . --raw-output)
     obj_md5=$(md5sum "$key" | awk '{ print $1 }')
-    if [ "$etag" == "$obj_md5" ]
-    then
+    if [ "$etag" == "$obj_md5" ]; then
       echo "$key content has not changed, skipping push"
     else
       eval $s3 put-object \
