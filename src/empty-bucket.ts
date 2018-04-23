951a1f1c59401fba91472b3605bf56ec520fa610
Mark Vayngrib <mark@tradle.io>
2018-04-23 14:30:16 -0400

    fix: empty buckets - loop till bucket is empty as AWS limits batch size

--- src/empty-bucket.ts
+++ src/empty-bucket.ts
@@ -11,37 +11,50 @@ export const emptyBucket = async (s3, Bucket) => {
     }).promise()
   }
 
-  let Versions
-  try {
-    // get the list of all objects in the bucket
-    ({ Versions } = await s3.listObjectVersions({ Bucket }).promise())
-  } catch (err) {
-    if (err.code === 'NoSuchBucket') return
+  const run = async () => {
+    let Versions
+    try {
+      // get the list of all objects in the bucket
+      ({ Versions } = await s3.listObjectVersions({ Bucket }).promise())
+    } catch (err) {
+      if (err.code === 'NoSuchBucket') return
 
-    throw err
+      throw err
+    }
+
+    let count = 0
+    if (Versions.length > 0) {
+      count += Versions.length
+      // if the bucket contains objects, delete them
+      logger.info(`Deleting ${Versions.length} object versions`)
+      await deleteObjects(Versions)
+    }
+
+    // check for any files marked as deleted previously
+    const { DeleteMarkers } = await s3.listObjectVersions({ Bucket }).promise()
+
+    if (DeleteMarkers.length > 0) {
+      count += DeleteMarkers.length
+      // if the bucket contains delete markers, delete them
+      logger.info(`Deleting ${DeleteMarkers.length} object delete markers`)
+      await deleteObjects(DeleteMarkers)
+    }
+
+    // if there are any non-versioned contents, delete them too
+    const { Contents } = await s3.listObjectsV2({ Bucket }).promise()
+
+    if (Contents.length > 0) {
+      count += Contents.length
+      // if the bucket contains delete markers, delete them
+      logger.info(`Deleting ${Contents.length} objects`)
+      await deleteObjects(Contents)
+    }
+
+    return count
   }
 
-  if (Versions.length > 0) {
-    // if the bucket contains objects, delete them
-    logger.info(`Deleting ${Versions.length} object versions`)
-    await deleteObjects(Versions)
-  }
-
-  // check for any files marked as deleted previously
-  const { DeleteMarkers } = await s3.listObjectVersions({ Bucket }).promise()
-
-  if (DeleteMarkers.length > 0) {
-    // if the bucket contains delete markers, delete them
-    logger.info(`Deleting ${DeleteMarkers.length} object delete markers`)
-    await deleteObjects(DeleteMarkers)
-  }
-
-  // if there are any non-versioned contents, delete them too
-  const { Contents } = await s3.listObjectsV2({ Bucket }).promise()
-
-  if (Contents.length > 0) {
-    // if the bucket contains delete markers, delete them
-    logger.info(`Deleting ${Contents.length} objects`)
-    await deleteObjects(Contents)
-  }
+  let count
+  do {
+    count = await run()
+  } while (count)
 }
