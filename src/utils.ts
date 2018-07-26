7ebe5bb770cb1b90f3da35500b050756fe558f5f
Mark Vayngrib <mark@tradle.io>
2018-07-26 09:30:53 -0400

    refactor: utils exports style

--- src/utils.ts
+++ src/utils.ts
@@ -21,7 +21,7 @@ type AWS = {
   cloudformation: _AWS.CloudFormation
 }
 
-const get = async (url) => {
+export const get = async (url) => {
   const res = await fetch(url)
   if (res.statusCode > 300) {
     throw new Error(res.statusText)
@@ -30,12 +30,12 @@ const get = async (url) => {
   return await res.json()
 }
 
-const getNamespace = ({ models, lenses }) => {
+export const getNamespace = ({ models, lenses }) => {
   const model = _.values(models)[0] || _.values(lenses)[0]
   return model && ModelsPack.getNamespace(model.id)
 }
 
-const pack = ({ namespace, models, lenses }: {
+export const pack = ({ namespace, models, lenses }: {
   namespace?: string
   models?: any
   lenses?: any
@@ -53,24 +53,24 @@ const pack = ({ namespace, models, lenses }: {
   return modelsPack
 }
 
-const validateResource = resource => _validateResource({
+export const validateResource = resource => _validateResource({
   models: builtInModels,
   resource
 })
 
-const prettify = obj => {
+export const prettify = obj => {
   return typeof obj === 'string' ? obj : JSON.stringify(obj, null, 2)
 }
 
-const isValidProjectPath = project => {
+export const isValidProjectPath = project => {
   return fs.existsSync(path.resolve(project, 'serverless.yml'))
 }
 
-const toEnvFile = obj => Object.keys(obj)
+export const toEnvFile = obj => Object.keys(obj)
   .map(key => `${key}="${obj[key]}"`)
   .join('\n')
 
-const confirmOrAbort = async (msg:string) => {
+export const confirmOrAbort = async (msg:string) => {
   const confirmed = await confirm(msg)
   if (!confirmed) {
     throw new CustomErrors.UserAborted()
@@ -78,7 +78,7 @@ const confirmOrAbort = async (msg:string) => {
 }
 
 const acceptAll = (item:any) => true
-const listStackResources = async (aws: AWS, StackName: string, filter=acceptAll) => {
+export const listStackResources = async (aws: AWS, StackName: string, filter=acceptAll) => {
   let resources = []
   const opts:any = { StackName }
   while (true) {
@@ -95,38 +95,38 @@ const listStackResources = async (aws: AWS, StackName: string, filter=acceptAll)
   return resources
 }
 
-const listStackResourcesByType = (type, aws: AWS, stackName:string) => {
+export const listStackResourcesByType = (type, aws: AWS, stackName:string) => {
   return listStackResources(aws, stackName, ({ ResourceType }) => type === ResourceType)
 }
 
-const listStackResourceIdsByType = async (type, aws: AWS, stackName:string) => {
+export const listStackResourceIdsByType = async (type, aws: AWS, stackName:string) => {
   const resources = await listStackResourcesByType(type, aws, stackName)
   return resources.map(r => r.PhysicalResourceId)
 }
 
-const listStackBuckets = listStackResourcesByType.bind(null, 'AWS::S3::Bucket')
-const listStackBucketIds = listStackResourceIdsByType.bind(null, 'AWS::S3::Bucket')
-const listStackFunctions = listStackResourcesByType.bind(null, 'AWS::Lambda::Function')
-const listStackFunctionIds = listStackResourceIdsByType.bind(null, 'AWS::Lambda::Function')
+export const listStackBuckets = listStackResourcesByType.bind(null, 'AWS::S3::Bucket')
+export const listStackBucketIds = listStackResourceIdsByType.bind(null, 'AWS::S3::Bucket')
+export const listStackFunctions = listStackResourcesByType.bind(null, 'AWS::Lambda::Function')
+export const listStackFunctionIds = listStackResourceIdsByType.bind(null, 'AWS::Lambda::Function')
 
-const destroyBucket = async (aws: AWS, Bucket: string) => {
+export const destroyBucket = async (aws: AWS, Bucket: string) => {
   await emptyBucket(aws.s3, Bucket)
   await aws.s3.deleteBucket({ Bucket }).promise()
 }
 
-const deleteStack = async (aws: AWS, StackName:string) => {
+export const deleteStack = async (aws: AWS, StackName:string) => {
   return await aws.cloudformation.deleteStack({ StackName }).promise()
 }
 
-const awaitStackDelete = async (aws: AWS, StackName:string) => {
+export const awaitStackDelete = async (aws: AWS, StackName:string) => {
   return await aws.cloudformation.waitFor('stackDeleteComplete', { StackName }).promise()
 }
 
-const awaitStackUpdate = async (aws: AWS, StackName:string) => {
+export const awaitStackUpdate = async (aws: AWS, StackName:string) => {
   return await aws.cloudformation.waitFor('stackUpdateComplete', { StackName }).promise()
 }
 
-const listStacks = async (aws) => {
+export const listStacks = async (aws) => {
   const listStacksOpts:any = {
     StackStatusFilter: ['CREATE_COMPLETE', 'UPDATE_COMPLETE']
   }
@@ -151,23 +151,23 @@ const listStacks = async (aws) => {
   return stackInfos
 }
 
-const getApiBaseUrl = async (aws: AWS, StackName:string) => {
+export const getApiBaseUrl = async (aws: AWS, StackName:string) => {
   const result = await aws.cloudformation.describeStacks({ StackName }).promise()
   const { Outputs } = result.Stacks[0]
   const endpoint = Outputs.find(x => /^ServiceEndpoint/.test(x.OutputKey))
   return endpoint.OutputValue
 }
 
-const splitCamelCase = str => str.split(/(?=[A-Z])/g)
-const checkCommandInPath = cmd => {
+export const splitCamelCase = str => str.split(/(?=[A-Z])/g)
+export const checkCommandInPath = cmd => {
   const { code } = shelljs.exec(`command -v ${cmd}`)
   if (code !== 0) {
     throw new CustomErrors.InvalidEnvironment(`Please install: ${cmd}`)
   }
 }
 
-const wait = millis => new Promise(resolve => setTimeout(resolve, millis))
-const normalizeNodeFlags = flags => {
+export const wait = millis => new Promise(resolve => setTimeout(resolve, millis))
+export const normalizeNodeFlags = flags => {
   if (!(flags.inspect || flags['inspect-brk'] || flags.debug || flags['debug-brk'])) return
 
   const nodeVersion = Number(process.version.slice(1, 2))
@@ -186,32 +186,6 @@ const normalizeNodeFlags = flags => {
       flags.inspect = true
     }
   }
-}
-
-export {
-  get,
-  getNamespace,
-  pack,
-  validateResource,
-  prettify,
-  isValidProjectPath,
-  toEnvFile,
-  confirmOrAbort,
-  listStacks,
-  listStackResources,
-  listStackBuckets,
-  listStackBucketIds,
-  listStackFunctions,
-  listStackFunctionIds,
-  destroyBucket,
-  deleteStack,
-  awaitStackDelete,
-  awaitStackUpdate,
-  getApiBaseUrl,
-  splitCamelCase,
-  checkCommandInPath,
-  wait,
-  normalizeNodeFlags
 }
 
 export const pickNonNull = obj => _.pickBy(obj, val => val != null)
