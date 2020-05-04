fd4ad12f012997b59fd4e27de88f6f343e74430a
jacobgins <jacob.gins@tradle.io>
2020-05-03 20:23:33 -0400

    kyc-in-vpn 1.2.1

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ import {
 } from './types'
 
 export const TRADLE_ACCOUNT_ID = '210041114155'
-export const SERVICES_STACK_TEMPLATE_URL = 'https://s3.eu-west-2.amazonaws.com/tradle.io/cf-templates/kyc-in-ecs/1.2.0/main.yml'
+export const SERVICES_STACK_TEMPLATE_URL = 'https://s3.eu-west-2.amazonaws.com/tradle.io/cf-templates/kyc-in-ecs/1.2.1/main.yml'
 export const REPO_NAMES: KYCServicesConf = {
   truefaceSpoof: 'trueface-spoof',
   rankOne: 'rank-one',
